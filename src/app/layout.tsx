a13ae98e15609bfe053e7550da698a54ca14241e
Matthew Onah <onahmattolom@gmail.com>
2025-08-12 16:19:37 +0000

    when i copied the link to my phone and saved it to home screen, it is th

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,8 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <link rel="icon" href="/logo.svg" type="image/svg+xml" />
+        <link rel="apple-touch-icon" href="/logo.svg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter&display=swap" rel="stylesheet" />
